79c6b9861975ce8e894cb9aaa8349b8c3ed502d9
Evgeniy Ko <evgeniyko@Evgeniys-MBP.attlocal.net>
2022-07-26 20:12:06 -0400

    creating packages, classes, models, and beginning service implementation

--- src/main/java/com/revature/alchemyapp/services/UserServiceImpl.java
+++ src/main/java/com/revature/alchemyapp/services/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.revature.alchemyapp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.revature.alchemyapp.data.CategoryRepository;
+import com.revature.alchemyapp.data.ShelfRepository;
+import com.revature.alchemyapp.data.UserRepository;
 import com.revature.alchemyapp.exceptions.UsernameAlreadyExistsException;
 import com.revature.alchemyapp.models.Category;
 import com.revature.alchemyapp.models.Shelf;
@@ -11,35 +15,63 @@ import com.revature.alchemyapp.models.User;
 
 @Service
 public class UserServiceImpl implements UserService {
-
+	private UserRepository userRepo;
+	private ShelfRepository shelfRepo;
+	private CategoryRepository categoryRepo;
+	
+	public UserServiceImpl(UserRepository userRepo, ShelfRepository shelfRepo, CategoryRepository categoryRepo) {
+		this.userRepo = userRepo;
+		this.shelfRepo = shelfRepo;
+		this.categoryRepo = categoryRepo;
+	}
 	@Override
 	public User registerUser(User user) throws UsernameAlreadyExistsException {
-		// TODO Auto-generated method stub
-		return null;
+		user.setId(0);
+		user = userRepo.save(user);
+		if (user.getId() == 0) {
+			throw new UsernameAlreadyExistsException();
+		}
+		return user;
 	}
 
 	@Override
 	public User logIn(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = userRepo.findByUsername(username);
+		if (user != null && (password != null && password.equals(user.getPassword()))) {
+			return user;
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public User getUser(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<User> userOpt = userRepo.findById(id);
+		if (userOpt.isPresent()) {
+			return userOpt.get();
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public User addBook(String bookISBN, User user, int category) {
-		// TODO Auto-generated method stub
-		return null;
+		if (user == null) {
+			return null;
+		} 
+		Shelf shelf = new Shelf(0, user.getId(), bookISBN, category);
+		List<Shelf> shelves = user.getShelves();
+		shelves.add(shelf);
+		user.setShelves(shelves);
+		
+		userRepo.save(user);
+		shelfRepo.save(shelf);
+		return user;
 	}
 
 	@Override
 	public List<Category> viewShelfCategories() {
-		// TODO Auto-generated method stub
-		return null;
+		return categoryRepo.findAll();
 	}
 
 	@Override
